211e94bfa66e926d12d3dd5519f80eb6289d3a3e
metin.gercek <metingercek@hotmail.com>
2023-01-10 14:31:02 +0200

    create frontend routes

---
+++ client/src/scenes/navbar/index.jsx
@@ -0,0 +1,5 @@
+const Navbar = () => {
+  return <div>Navbar Page</div>;
+};
+
+export default Navbar;
---
+++ client/src/scenes/profilePage/index.jsx
@@ -0,0 +1,6 @@
+const ProfilePage = () => {
+    return <div>Profile Page</div>;
+  };
+  
+  export default ProfilePage;
+  
