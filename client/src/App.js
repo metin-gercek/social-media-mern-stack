ec58fb1aadebd383ff1499abda2c97eb7bc687db
metin.gercek <metingercek@hotmail.com>
2023-01-10 14:31:48 +0200

    create frontend routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,16 @@ import HomePage from "scenes/homePage";
 import LoginPage from "scenes/loginPage";
 import ProfilePage from "scenes/profilePage";
 
-
-
 function App() {
-  
   return (
     <div className="app">
-     <BrowserRouter>
-     <Routes>
-      <Route path="/" element={LoginPage} />
-      <Route path="/home" element={HomePage} />
-      <Route path="/profile/:userId" element={ProfilePage} />
-     </Routes>
-     </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={LoginPage} />
+          <Route path="/home" element={HomePage} />
+          <Route path="/profile/:userId" element={ProfilePage} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
