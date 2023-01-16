4524bf03454fd6c5c70d4526c862706fb7f6820f
metin.gercek <metingercek@hotmail.com>
2023-01-16 18:27:16 +0200

    create widgets

--- client/src/components/WidgetWrapper.jsx
+++ client/src/components/WidgetWrapper.jsx
@@ -1,10 +1,10 @@
 import { Box } from "@mui/material";
 import { styled } from "@mui/system";
 
-const WidgetWrapper = styled()(({theme}) => ({
-    padding: "1.5rem 1.5rem 0.75rem 1.5rem",
-    backgroundColor: theme.pallette.background.alt,
-    borderRadius: "0.75rem"
+const WidgetWrapper = styled(Box)(({ theme }) => ({
+  padding: "1.5rem 1.5rem 0.75rem 1.5rem",
+  backgroundColor: theme.palette.background.alt,
+  borderRadius: "0.75rem",
 }));
 
-export default WidgetWrapper
+export default WidgetWrapper;
