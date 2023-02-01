af3230e7d60a3d7ceff7383af482e52f7026fe94
metin.gercek <metingercek@hotmail.com>
2023-02-01 22:31:41 +0200

    fix deploy errors

--- client/src/components/Userimage.jsx
+++ client/src/components/Userimage.jsx
@@ -4,7 +4,7 @@ const UserImage = ({ image, size = "60px" }) => {
   return (
     <Box width={size} height={size}>
       <img
-        style={{ objectFit: "cover", borderRadius: "50px" }}
+        style={{ objectFit: "cover", borderRadius: "50%" }}
         width={size}
         height={size}
         alt="user"
