817848ca99b18aa89782737e51b88981e0a1c067
metin.gercek <metingercek@hotmail.com>
2023-02-01 21:37:02 +0200

    add proxy address

--- client/src/components/Userimage.jsx
+++ client/src/components/Userimage.jsx
@@ -8,7 +8,7 @@ const UserImage = ({ image, size = "60px" }) => {
         width={size}
         height={size}
         alt="user"
-        src={`/assets/${image}`}
+        src={`https://social-media-mern-stack-h7w1.onrender.com/assets/${image}`}
       />
     </Box>
   );
